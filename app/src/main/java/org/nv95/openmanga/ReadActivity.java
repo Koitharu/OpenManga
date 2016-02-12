6fde0238fb07d138f84fff5f57cf0ea725fb795e
nv95 <nvasya95@gmail.com>
2016-02-12 16:20:08 +0200

    Fix fullscreen reader mode

--- app/src/main/java/org/nv95/openmanga/ReadActivity.java
+++ app/src/main/java/org/nv95/openmanga/ReadActivity.java
@@ -252,15 +252,13 @@ public class ReadActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
+    protected void onStart() {
+        super.onStart();
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-            if (hasFocus) {
-                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-            }
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
 
