d2acdeed539e31f832dc3dcc168375b630fc3f74
nv95 <nvasya95@gmail.com>
2016-02-12 22:09:26 +0200

    Helper class for saving manga

--- app/src/main/java/org/nv95/openmanga/helpers/MultiThreadHelper.java
+++ app/src/main/java/org/nv95/openmanga/helpers/MultiThreadHelper.java
@@ -1,7 +1,36 @@
 package org.nv95.openmanga.helpers;
 
+import android.os.AsyncTask;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by nv95 on 12.02.16.
  */
-public class MultiThreadHelper {
+public class MultiThreadHelper <T extends AsyncTask> {
+    private final int mThreadsCount;
+    private final BlockingQueue<Runnable> mDecodeWorkQueue;
+    private final ThreadPoolExecutor mThreadPool;
+
+    public MultiThreadHelper(int preferredCount) {
+        mDecodeWorkQueue = new LinkedBlockingQueue<Runnable>();
+        mThreadsCount = Math.min(preferredCount, Runtime.getRuntime().availableProcessors());
+        mThreadPool = new ThreadPoolExecutor(
+                mThreadsCount,       // Initial pool size
+                mThreadsCount,       // Max pool size
+                1,                   // The amount of time an idle thread waits before terminating
+                TimeUnit.SECONDS,
+                mDecodeWorkQueue);
+    }
+
+
+
+    public void addTask(T task) {
+        //noinspection unchecked
+        task.executeOnExecutor(mThreadPool);
+    }
+
 }
--- app/src/main/java/org/nv95/openmanga/items/SimpleDownload.java
+++ app/src/main/java/org/nv95/openmanga/items/SimpleDownload.java
@@ -1,7 +1,71 @@
 package org.nv95.openmanga.items;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by nv95 on 12.02.16.
  */
-public class SimpleDownload {
+public class SimpleDownload implements Runnable {
+    private final String mSourceUrl;
+    private final File mDestination;
+
+    public SimpleDownload(String sourceUrl, File destination) {
+        this.mSourceUrl = sourceUrl;
+        this.mDestination = destination;
+    }
+
+    @Override
+    public void run() {
+        InputStream input = null;
+        OutputStream output = null;
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(mSourceUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                return;
+            }
+            input = connection.getInputStream();
+            output = new FileOutputStream(mDestination);
+            byte data[] = new byte[4096];
+            int count;
+            while ((count = input.read(data)) != -1) {
+                output.write(data, 0, count);
+                //if (Thread.interrupted())
+            }
+        } catch (Exception e) {
+            if (mDestination.exists()) {
+                mDestination.delete();
+            }
+        } finally {
+            try {
+                if (output != null)
+                    output.close();
+                if (input != null)
+                    input.close();
+            } catch (IOException ignored) {
+            }
+            if (connection != null)
+                connection.disconnect();
+        }
+    }
+
+    public boolean isSuccess() {
+        return mDestination.exists();
+    }
+
+    public File getDestination() {
+        return mDestination;
+    }
+
+    public String getSourceUrl() {
+        return mSourceUrl;
+    }
 }
