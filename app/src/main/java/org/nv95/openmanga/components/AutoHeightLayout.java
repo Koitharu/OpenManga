dc68d62403b05c975e299b8729e2bb10d2498697
nv95 <nvasya95@gmail.com>
2016-04-04 13:04:27 +0300

    Update libs and fix grid

---
+++ app/src/main/java/org/nv95/openmanga/components/AutoHeightLayout.java
@@ -0,0 +1,41 @@
+package org.nv95.openmanga.components;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by nv95 on 25.01.16.
+ */
+public class AutoHeightLayout extends FrameLayout {
+    private static final double ASPECT_RATIO = 5f / 3f;
+
+    public AutoHeightLayout(Context context) {
+        super(context);
+    }
+
+    public AutoHeightLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoHeightLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AutoHeightLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int originalWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int calculatedHeight = (int) (originalWidth * ASPECT_RATIO);
+        super.onMeasure(
+                MeasureSpec.makeMeasureSpec(originalWidth, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(calculatedHeight, MeasureSpec.EXACTLY)
+        );
+    }
+}
