e413526340880fb7ca4703a0cf2770dc536b7fdd
nv95 <nvasya95@gmail.com>
2016-11-17 19:55:31 +0200

    Reader improvements

--- app/src/main/java/org/nv95/openmanga/components/reader/MangaReader.java
+++ app/src/main/java/org/nv95/openmanga/components/reader/MangaReader.java
@@ -2,22 +2,20 @@ package org.nv95.openmanga.components.reader;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.view.View;
+
+import org.nv95.openmanga.components.reader.recyclerpager.PreCachingLayoutManager;
+import org.nv95.openmanga.components.reader.recyclerpager.RecyclerViewPager;
 
 /**
  * Created by nv95 on 15.11.16.
  */
 
-public class MangaReader extends RecyclerView {
-
-    private static final float SCROLL_FACTOR = 1f;
+public class MangaReader extends RecyclerViewPager {
 
     @Nullable
-    private LinearLayoutManager mLayoutManager;
-    private boolean mSticky;
+    private PreCachingLayoutManager mLayoutManager;
 
     public MangaReader(Context context) {
         super(context);
@@ -36,35 +34,6 @@ public class MangaReader extends RecyclerView {
 
     private void init(Context context) {
         mLayoutManager = null;
-        mSticky = true;
-        addOnScrollListener(new OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if(mSticky && newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    completeScroll();
-                }
-            }
-        });
-    }
-
-    public int getCurrentPosition() {
-        if (mLayoutManager == null) {
-            return RecyclerView.NO_POSITION;
-        }
-        int lastPos = mLayoutManager.findLastVisibleItemPosition();
-        View lastView = mLayoutManager.findViewByPosition(lastPos);
-        if (lastView == null) {
-            return RecyclerView.NO_POSITION;
-        }
-        if (isVertical()) {
-            int center = getHeight() / 2;
-            lastPos = lastView.getTop() < center ? lastPos : lastPos - 1;
-        } else {
-            int center = getWidth() / 2;
-            lastPos = lastView.getLeft() < center ? lastPos : lastPos - 1;
-        }
-        return lastPos < 0 ? 0 : lastPos;
     }
 
     private void completeScroll() {
@@ -75,16 +44,17 @@ public class MangaReader extends RecyclerView {
     }
 
     public void applyConfig(boolean vertical, boolean reverse, boolean sticky) {
-        setLayoutManager(mLayoutManager = new LinearLayoutManager(
+        setLayoutManager(mLayoutManager = new PreCachingLayoutManager(
                 getContext(),
-                vertical ? LinearLayoutManager.VERTICAL : LinearLayoutManager.HORIZONTAL,
+                vertical ? PreCachingLayoutManager.VERTICAL : PreCachingLayoutManager.HORIZONTAL,
                 reverse
         ));
-        mSticky = sticky;
+        setSinglePageFling(sticky);
+        setSticky(sticky);
     }
 
     public boolean isVertical() {
-        return mLayoutManager != null && mLayoutManager.getOrientation() == LinearLayoutManager.VERTICAL;
+        return mLayoutManager != null && mLayoutManager.getOrientation() == PreCachingLayoutManager.VERTICAL;
     }
 
     public boolean isReversed() {
